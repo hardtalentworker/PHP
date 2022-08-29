5451cfcb03f93c928931360e0d1ff5d6b1275082
dvvcobr <50865630+dvvcobr@users.noreply.github.com>
2022-08-29 21:16:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,56 @@
 </head>
 <body>
 	<?php
+	if(isset($_POST['fsubmit'])){
+			if(isset($_POST['name'])){
+				setcookie('fio',$_POST['name'].' '.$_POST['fam']);
+			}
+			header('location: '.$_SERVER['PHP_SELF']);
+			exit();
+		}
+		if(isset($_POST['freset'])){
+			setcookie ('fio', '', time() - 3600);
+			header('location: '.$_SERVER['PHP_SELF']);
+			exit();
+		}
+		
+		//print_r($_ENV);
+		//echo '<br><br>';
+		echo($_ENV['ENVIRONMENT']);
+		echo '<br><br>';
+		
+		if(!isset($_ENV['ENVIRONMENT'])){
+			echo '<p><font color="red">Режим разработки</font></p><br><br>';
+		}else{
+			if($_ENV['ENVIRONMENT']=='test'){
+				echo '<p><font color="blue">Режим тестирования</font></p><br><br>';
+			}elseif($_ENV['ENVIRONMENT']=='production'){
+				echo '<p><font color="green">Режим эксплуатации</font></p><br><br>';
+			}
+		}
+	
+		if(isset($_COOKIE['fio'])){
+			if((date('H')>=5)&(date('H')<11)){
+				echo 'Доброе утро, '.$_COOKIE['fio'].'<br>';
+			}elseif((date('H')>=11)&(date('H')<16)){
+				echo 'Добрый день, '.$_COOKIE['fio'].'<br>';
+			}elseif(date('H')>=16){
+				echo 'Добрый вечер, '.$_COOKIE['fio'].'<br>';
+			}elseif(date('H')<5){
+				echo 'Доброй ночи, '.$_COOKIE['fio'].'<br>';
+			}
+			echo '<br><br>';
+			echo "<form method='post' enctype='multipart/form-data'>";
+			echo "<input type='submit' name='freset' value='reset'><br>";
+			echo "</form>";
+		}else{
+			echo "<form method='post' enctype='multipart/form-data'>";
+			echo "Имя <input type='text' name='name' maxlength='50' size='10' value=''><br>";
+			echo "Фамилия <input type='text' name='fam' maxlength='50' size='10' value=''><br>";
+			echo "<input type='submit' name='fsubmit' value='fsubmitText'><br><br>";
+			echo"</form>";
+		}
+	
 		$arr=explode(' ',$_SERVER['HTTP_USER_AGENT']);
 		$arr1=explode('/',$arr[8]);
 		if($arr1[0]!='Gecko'){
