d3a0cbd5a609da42015ab7872ae257fd9861edfc
dvvcobr <50865630+dvvcobr@users.noreply.github.com>
2022-09-06 20:33:25 +0500

    Update exec.php

--- exec.php
+++ exec.php
@@ -1,14 +1,30 @@
 <?php
-    if(!empty($_POST)){
-        $file=fopen("list.txt","r");
-        $i=1;
-        if($file){
-            while(($str=fgets($file))!==FALSE){
-                //echo 'checkbox{$i} '.$_POST['fcheckbox{$i}'];
-                echo "<input type='checkbox' name='fcheckbox{$i}' value='{$i}' checked>{$str}";
-                echo '<br>';
+    function file_refresh($file,array $param,$num_uni){ //$file - файл $param - массив $num_uni - номер уникального элемента
+        $arr_temp=[];
+        $i=0;
+        
+        if(($handle=fopen($file, "a+"))!== FALSE){
+            while (($data = fgetcsv($handle, 1000, ";")) != null) {
+                for ($j=0;$j<count($data);$j++) {
+                    $arr_temp[$i]=$data[$j];
+                    $i++;
+                }
+            }
+            fclose($handle);
+        }
+        
+        if(!in_array($param[$num_uni],$arr_temp)){
+            for ($j=0;$j<count($param);$j++) {
+                $arr_temp[$i]=$param[$j];
                 $i++;
             }
         }
+        
+        $handle = fopen($file, 'w');
+        fputcsv($handle,$arr_temp,";");
+        fclose($handle);
+        
+        unset($data);
+        unset($arr_temp);
     }
 ?>
