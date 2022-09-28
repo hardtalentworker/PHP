e23e063c002a25bfdae54d4e40b9db6feff89ef8
dvvcobr <50865630+dvvcobr@users.noreply.github.com>
2022-09-28 17:09:32 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -265,3 +265,43 @@ $arr=[];
 		
 		return implode($fin);
 	}
+//*****************************************************************    сохранение в файл массива из параметров с проверкой на уникальность
+function file_refresh($file,array $param,$num_uni){ //$file - файл $param - массив $num_uni - номер уникального элемента
+        $result=0; //такой уже есть
+        $arr_temp=[];
+        $i=0;
+        
+        if(($handle=fopen($file, "a+"))!== FALSE){
+            while (($data = fgetcsv($handle, 1000, ";")) != null) {
+                for ($j=0;$j<count($data);$j++) {
+                    $arr_temp[$i]=$data[$j];
+                    $i++;
+                }
+            }
+            fclose($handle);
+        }
+        
+        if(!in_array($param[$num_uni],$arr_temp)){
+            for ($j=0;$j<count($param);$j++) {
+                $arr_temp[$i]=$param[$j];
+                $i++;
+                $result=1; //успешно добавлен
+            }
+        }
+        
+        $handle = fopen($file, 'w');
+        $ii=0;
+        while($ii<$i){
+            $arr_temp2=[];
+            for ($j=0;$j<count($param);$j++) {
+                $arr_temp2[$j]=$arr_temp[$ii];
+                $ii++;
+            }
+            fputcsv($handle,$arr_temp2,";");
+        }
+        fclose($handle);
+        
+        unset($data);
+        unset($arr_temp);
+        
+        return $result;
